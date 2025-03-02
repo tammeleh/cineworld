cc06809c1b4a0a5e458cfe3938bae35896e2d627
tammeleh <georg.gert.tammeleht@gmail.com>
2025-03-02 17:53:58 +0700

    Break up MovieList page into an component with props and parent component that handles the data so it can be unit tested

---
+++ src/components/MovieListTemplate.tsx
@@ -0,0 +1,85 @@
+import MovieCardListSkeleton from '@/components/skeletons/MovieCardListSkeleton'
+import SearchInput from '@/components/SearchInput'
+import Pagination from '@/components/Pagination'
+import { TmdbListResponse } from '@/types/tmdb'
+import CardMovie from '@/components/CardMovie'
+import PageTitle from '@/components/PageTitle'
+import { ChangeEvent } from 'react'
+import clsx from 'clsx'
+
+interface MovieListTemplateProps {
+  onSearchChange: (e: ChangeEvent<HTMLInputElement>) => void
+  onPageChange: (page: number) => void
+  data?: TmdbListResponse
+  searchQuery: string
+  currentPage: number
+  isLoading: boolean
+  totalPages: number
+  error?: string
+}
+
+const MovieListTemplate = ({
+  onSearchChange,
+  onPageChange,
+  searchQuery,
+  currentPage,
+  totalPages,
+  isLoading,
+  error,
+  data,
+}: MovieListTemplateProps) => {
+  console.log(data)
+  const hasResults = data && data.results && data.results.length > 0
+
+  return (
+    <>
+      <PageTitle>
+        {searchQuery.trim().length > 0 ? 'Search Results' : 'Discover Movies'}
+        <SearchInput onChange={onSearchChange} value={searchQuery} />
+      </PageTitle>
+      {isLoading && <MovieCardListSkeleton />}
+
+      {error ? (
+        <div>Error: {error}</div>
+      ) : (
+        <>
+          {!hasResults && !isLoading && searchQuery.trim().length > 0 && (
+            <div className="mt-12 flex flex-col gap-8 text-center md:mt-12">
+              <div className="text-5xl md:text-9xl" aria-label="Shrug">
+                ¯\_(ツ)_/¯
+              </div>
+              <p className="text-xs text-gray-500 md:text-base">
+                Found no results for "{searchQuery}"
+              </p>
+            </div>
+          )}
+
+          {hasResults && !isLoading && (
+            <ul className="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-4 lg:grid-cols-3">
+              {data!.results.map((movie) => (
+                <li key={movie.id}>
+                  <CardMovie
+                    imageId={movie.backdrop_path}
+                    rating={movie.vote_average}
+                    overview={movie.overview}
+                    title={movie.title}
+                    movieId={movie.id}
+                  />
+                </li>
+              ))}
+            </ul>
+          )}
+
+          <Pagination
+            className={clsx('mt-4 md:mt-8', isLoading && 'pointer-events-none')}
+            onPageChange={onPageChange}
+            currentPage={currentPage}
+            totalPages={totalPages}
+          />
+        </>
+      )}
+    </>
+  )
+}
+
+export default MovieListTemplate
--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -1,12 +1,7 @@
-import MovieCardListSkeleton from '@/components/skeletons/MovieCardListSkeleton'
-import { useEffect, useState, useMemo } from 'react'
-import SearchInput from '@/components/SearchInput'
-import Pagination from '@/components/Pagination'
-import CardMovie from '@/components/CardMovie'
-import PageTitle from '@/components/PageTitle'
+import MovieListTemplate from '@/components/MovieListTemplate'
+import React, { useEffect, useState, useMemo } from 'react'
 import useDebounce from '@/hooks/useDebounce'
 import useMovies from '@/hooks/useMovies'
-import clsx from 'clsx'
 
 const MovieList = () => {
   const [discoverPage, setDiscoverPage] = useState(1)
@@ -39,10 +34,9 @@ const MovieList = () => {
   const { isLoading, error, data } = useMovies(queryParams)
 
   // Tmdb returns maximum of 500 pages and each page has 20 movies.
-  // On ui we show 10 movies per page as per the requirements.
+  // On UI we show 10 movies per page as per the requirements.
   const maxUiPages = 1000
   const validTotalPages = data ? Math.min(data.total_pages, maxUiPages) : 0
-  const hasResults = data && data.results.length > 0
 
   const handlePaginationChange = (newPage: number) => {
     if (isSearching) {
@@ -53,51 +47,17 @@ const MovieList = () => {
   }
   const paginationCurrentPage = isSearching ? searchPage : discoverPage
 
-  if (error) return <div>Error: {error.message}</div>
-
   return (
-    <>
-      <PageTitle>
-        {isSearching ? 'Search Results' : 'Discover Movies'}
-        <SearchInput onChange={handleSearchChange} value={searchQuery} />
-      </PageTitle>
-
-      {!hasResults && !isLoading && isSearching && (
-        <div className="mt-12 flex flex-col gap-8 text-center md:mt-12">
-          <div className="text-5xl md:text-9xl" aria-label="Shrug">
-            ¯\_(ツ)_/¯
-          </div>
-          <p className="text-xs text-gray-500 md:text-base">
-            Found no results for "{debouncedSearchQuery}"
-          </p>
-        </div>
-      )}
-
-      {hasResults && !isLoading && (
-        <ul className="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-4 lg:grid-cols-3">
-          {data.results.map((movie) => (
-            <li key={movie.id}>
-              <CardMovie
-                imageId={movie.backdrop_path}
-                rating={movie.vote_average}
-                overview={movie.overview}
-                title={movie.title}
-                movieId={movie.id}
-              />
-            </li>
-          ))}
-        </ul>
-      )}
-
-      {isLoading && <MovieCardListSkeleton />}
-
-      <Pagination
-        className={clsx('mt-4 md:mt-8', isLoading && 'pointer-events-none')}
-        onPageChange={handlePaginationChange}
-        currentPage={paginationCurrentPage}
-        totalPages={validTotalPages}
-      />
-    </>
+    <MovieListTemplate
+      error={error ? error.message : undefined}
+      onPageChange={handlePaginationChange}
+      onSearchChange={handleSearchChange}
+      currentPage={paginationCurrentPage}
+      totalPages={validTotalPages}
+      searchQuery={searchQuery}
+      data={data || undefined}
+      isLoading={isLoading}
+    />
   )
 }
 
